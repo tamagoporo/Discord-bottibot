46b5de2341d3ce33152f8a99477b06b5de413d67
poro <tamagoporo0920@yahoo.co.jp>
2023-02-24 23:29:28 +0900

    GCPのAppEngine用のモジュール追加、logger調整

--- discord_bottibot/logger.py
+++ discord_bottibot/logger.py
@@ -1,7 +1,13 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
 import os
 import sys
+from enum import Enum
+import google.cloud.logging
+
+
+class LogOutputType(Enum):
+    STDOUT = 1
+    GOOGLE = 2
 
 
 class Logger(object):
@@ -10,8 +16,8 @@ class Logger(object):
         
     @classmethod
     def setup(self):
-        # ログのフォーマットを設定
-        LOG_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
+        
+        log_output_type = LogOutputType.GOOGLE
 
         # ログファイル名を指定
         LOG_FILE = "./log/bottibot.log"
@@ -26,21 +32,29 @@ class Logger(object):
         logging.addLevelName(logging.ERROR, "ERR")
         logging.addLevelName(logging.CRITICAL, "CRT")
 
-        # ログファイル出力(ローテーション)用ハンドラの作成
-        rotation_file_handler = TimedRotatingFileHandler(LOG_FILE, when="midnight", interval=1, backupCount=7)
-        rotation_file_handler.suffix="_%Y%m%d.log"
-        rotation_file_handler.setFormatter(logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S'))
-
-        # 標準出力用ハンドラの作成
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setFormatter(logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S'))
-
         # ロガーを作成
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-        logger.addHandler(rotation_file_handler)
-        logger.addHandler(stdout_handler)
         self._logger = logger
+
+        if log_output_type == LogOutputType.STDOUT:
+            self._setup_stdout_logging(logger)
+            return
+        if log_output_type == LogOutputType.GOOGLE:
+            self._setup_google_logging()
+            return
+
+    # 標準出力用ハンドラの作成
+    def _setup_stdout_logging(logger):
+        LOG_FORMAT = "%(asctime)s [%(levelname)s] %(message)s"
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setFormatter(logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S'))
+        logger.addHandler(stdout_handler)
+
+    # 標準のloggingモジュールにgoogle.loggingを統合
+    def _setup_google_logging():
+        client = google.cloud.logging.Client()
+        client.setup_logging()
 
     @classmethod
     def log_e(self, log):
--- discord_bottibot/main.py
+++ discord_bottibot/main.py
@@ -1,8 +1,25 @@
 from os import getenv
+from threading import Thread
+import time
+from flask import Flask
 from dotenv import load_dotenv
 import context
 import bottibot
 from logger import Logger
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return 'Hello World!'
+
+
+@app.route('/_ah/warmup')
+def warmup():
+    # Handle your warmup logic here, e.g. set up a database connection pool
+    return '', 200, {}
 
 
 def setup(ctx):
@@ -21,7 +38,19 @@ def setup(ctx):
     Logger.setup()
 
 
+def start_thread():
+    t = Thread(target=bottibot.run, args=(ctx, ), daemon=True)
+    t.start()
+
+
 if __name__ == "__main__":
     ctx = context.Context()
     setup(ctx)
-    bottibot.run(ctx)
+    start_thread()
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080)
+    while True:
+        time.sleep(0.1)
+    
